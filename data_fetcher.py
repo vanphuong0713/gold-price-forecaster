56b34c5bb7dc215393cdd842d214ce17320f6571
Vo Phuong <35993931+vanphuong0713@users.noreply.github.com>
2025-03-11 14:53:42 +0700

    data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
 
 def fetch_gold_price_data():
-    # Lấy dữ liệu giá vàng (GC=F là mã vàng tương lai)
-    gold = yf.download('GC=F', start='2023-01-01', end='2025-03-10', progress=False)
-    return gold['Close']  # Chỉ lấy giá đóng cửa
+    today = datetime.now().strftime('%Y-%m-%d')
+    gold = yf.download('GC=F', start='2023-01-01', end=today, progress=False)
+    return gold['Close']
